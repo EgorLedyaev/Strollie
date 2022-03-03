6765fabcad713a31b21aaffede31e59be01df4c3
Egor <egorledyaev1@gmail.com>
2022-03-04 00:43:48 +0400

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,61 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(StrollieApp());
+void main() => runApp(Strollie());
 
 const bg_color_green = const Color(0xFF1BE07C);
-bool showlogo = false;
 
-class StrollieApp extends StatelessWidget{
+class Strollie extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Main_screen(),
+    );
+  }
+}
+
+class Main_screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: bg_color_green,
-        body: Center(
-          child: Image(
-            image: AssetImage('assets/images/Strollie_logo.png'),
-            height: 550
-          ),
-        )
+    return MaterialApp(
+      title: 'Strolly',
+      debugShowCheckedModeBanner: true, // плашка дебага
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: MapScreen(),
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  static const _initialCameraPosition = CameraPosition(
+      target: LatLng(53.228992, 50.199425),
+      zoom: 16,
+  );
+
+  //GoogleMapController _googleMapController;
+
+  @override
+  void dispose(){
+    //_googleMapController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: _initialCameraPosition,
+        //onMapCreated: (controller) => _googleMapController = controller,
       )
     );
   }
-
 }
