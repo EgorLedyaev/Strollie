d2ba5918cbe49ca81bc632a90abeeec5d3fb0326
Egor <egorledyaev1@gmail.com>
2022-03-06 23:44:51 +0400

    .

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,27 @@ class Main_screen extends StatelessWidget {
       title: 'Strolly',
       debugShowCheckedModeBanner: true, // плашка дебага
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primaryColor: Colors.transparent,
       ),
       home: MapScreen(),
+    );
+  }
+}
+
+class google_map_page extends StatelessWidget{
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(53.228992, 50.199425),
+    zoom: 16,
+  );
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: _initialCameraPosition,
+      ),
     );
   }
 }
@@ -36,75 +54,56 @@ class MapScreen extends StatefulWidget {
   _MapScreenState createState() => _MapScreenState();
 }
 
+class nav_menu extends StatefulWidget{
+  @override
+  _nav_menu_state createState() => _nav_menu_state();
+}
+class _nav_menu_state extends State<nav_menu>{
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      padding: EdgeInsets.only(bottom:50,left: 20,right:20),
+      constraints: const BoxConstraints(
+        minWidth: 380,
+        minHeight: 80,
+        maxWidth: 500,
+        maxHeight: 90,
+      ),
+        decoration: const BoxDecoration(
+          borderRadius : BorderRadius.all(Radius.circular(40)),
+          color : Color.fromRGBO(255, 255, 255, 1),
+        )
+    );
+  }
+}
+
+class search_profile_bar extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
 class _MapScreenState extends State<MapScreen> with SingleTickerProviderStateMixin {
-  static const _initialCameraPosition = CameraPosition(
-      target: LatLng(53.228992, 50.199425),
-      zoom: 16,
-  );
-
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2,vsync: this);
-  }
-
-  @override
-  void dispose(){
-    //_googleMapController.dispose();
-    super.dispose();
-    _tabController.dispose();
-  }
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: const GoogleMap(
-        myLocationButtonEnabled: false,
-        zoomControlsEnabled: false,
-        initialCameraPosition: _initialCameraPosition,
-      ),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.all(16.0),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(50.0)
-          ),
-          child: Container(
-            color: Colors.green,
-              child: TabBar(
-                labelColor: Color(0xFFC41A3B),
-                unselectedLabelColor: Colors.white,
-                labelStyle: TextStyle(fontSize: 10.0),
-                indicator: const UnderlineTabIndicator(
-                  borderSide: BorderSide(color: Colors.black54, width: 0.0),
-                  insets: EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 40.0),
-                ),
-                indicatorColor: Colors.black54,
-                tabs: const <Widget>[
-              Tab(
-              icon: Icon(
-                Icons.home,
-                size: 24.0,
-              ),
-            text: 'Home',
-          ),
-          Tab(
-            icon: Icon(
-              Icons.category,
-              size: 24.0,
-            ),
-            text: 'Department',
-          ),
-          ],
-                controller: _tabController,
-            )
-          )
-        )
-      ),
+    return Stack(
+      children: <Widget>[
+        Container(
+          child: google_map_page(),
+        ),
+        Container(
+          alignment: Alignment.bottomCenter,
+          child: nav_menu(),
+        ),
+      ],
     );
   }
 
 
 }
+
+
